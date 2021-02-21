a61a49b048e8f6b1c8b481902894400933bf836a
jimblu <jimmy.pezp@gmail.com>
2021-02-22 00:12:51 +0100

    updates

---
+++ todo/build/precache-manifest.4c10eb6700de2254e133052fb7eb981f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f129eb42f43a61bee8921ca6438368",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cdd53ee2904768a4b07",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f45827c21e90c9c42c67",
+    "url": "/static/css/main.b8899e1d.chunk.css"
+  },
+  {
+    "revision": "4cdd53ee2904768a4b07",
+    "url": "/static/js/2.4d9cc39c.chunk.js"
+  },
+  {
+    "revision": "f45827c21e90c9c42c67",
+    "url": "/static/js/main.0c0c5de3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
